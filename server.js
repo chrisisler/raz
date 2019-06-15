41368224886cc1d30ce7a328eb2869417eaa4d4c
chrisisler <christopherisler1@gmail.com>
2019-06-15 00:51:27 -0400

    push

---
+++ server.js
@@ -0,0 +1,11 @@
+let express = require('express')
+let path = require('path')
+let app = express()
+
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/', (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+app.listen(9000)
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import React, { useState, ReactNode, useEffect } from 'react'
+import { MemoryRouter, Route, Link, RouteComponentProps } from 'react-router-dom'
 
 import cached from 'one-cache'
 import './App.css'
@@ -52,6 +52,10 @@ interface FullContent extends Content {
   Response: string
 }
 
+const route = (url: string): void => {
+  window.history.pushState(null, '', url)
+}
+
 const createUrl = (params: string) =>
   `http://www.omdbapi.com?apikey=${process.env.REACT_APP_OMDB_API_KEY}${params}`
 
@@ -65,83 +69,139 @@ const ContentContainer = (props: { contents: Content[]; format: Format }) => {
       <h2 className="App-media-title">{props.format}</h2>
       {props.contents.map(({ Title, Year, imdbID, Type }: Content) => {
         return (
-          <li
-            className="App-media-item"
-            key={imdbID}
-            onClick={async (): Promise<void> => {
-              let endpoint = createUrl(`&i=${imdbID}&type=${Type}`)
-              try {
-                let data: FullContent = await cached(imdbID, async () => {
-                  return (await fetch(endpoint)).json()
-                })
-                console.log('data is:', data)
-              } catch (error) {
-                console.error('Error:', error.message)
-              }
-            }}
-          >
-            <div>
-              <strong>{Title}</strong>
-            </div>
-            {Year}
-          </li>
+          <Link to={`/${Type}/${imdbID}`} className="App-media-item-link" key={imdbID}>
+            <li className="App-media-item">
+              <div>
+                <strong>{Title}</strong>
+              </div>
+              {Year}
+            </li>
+          </Link>
         )
       })}
     </ul>
   )
 }
 
+function FullContentView(
+  props: RouteComponentProps<{ imdbID: string; type: string }>
+): JSX.Element {
+  let [loading, setLoading] = useState<boolean>(true)
+  let [data, setData] = useState<FullContent | null>(null)
+  let [error, setError] = useState<Error | null>(null)
+  let { imdbID, type } = props.match.params
+  useEffect(() => {
+    let cancelled = false
+    async function effect(): Promise<void> {
+      try {
+        let data: FullContent = await cached(imdbID, async () => {
+          let endpoint = createUrl(`&i=${imdbID}&type=${type}`)
+          return (await fetch(endpoint)).json()
+        })
+        console.log('data is:', data)
+        if (!cancelled) {
+          setData(data)
+          setError(null)
+        }
+      } catch (error) {
+        if (!cancelled) {
+          setError(error)
+          setData(null)
+        }
+      } finally {
+        if (!cancelled) {
+          setLoading(false)
+        }
+      }
+    }
+    effect()
+    return () => {
+      cancelled = true
+    }
+  }, [imdbID, type])
+
+  if (loading) {
+    return <h1>Loading...</h1>
+  } else if (error) {
+    return <h1>Error</h1>
+  }
+  return <h1>Success: {JSON.stringify(data, null, 2)}</h1>
+}
+
 export default function App(): JSX.Element {
   let [movies, setMovies] = useState<Content[]>([])
   let [serieses, setSerieses] = useState<Content[]>([])
   let [episodes, setEpisodes] = useState<Content[]>([])
-  let [display, setDisplay] = useState<boolean>(
+  let [display] = useState<boolean>(
     movies.length === 0 && serieses.length === 0 && episodes.length === 0
   )
+  let [search, setSearch] = useState('')
+  let [total, setTotal] = useState(0)
 
   return (
-    <main className="App">
-      <header className="App-header">
-        <h1>raz</h1>
-      </header>
-      <input
-        type="text"
-        className="App-search"
-        placeholder="Find movies, shows, and people..."
-        onKeyUp={async (event): Promise<void> => {
-          let key: string = event.key
-          let searched: string = event.currentTarget.value
-          if (key === 'Enter') {
-            try {
-              let endpoint = createUrl(`&s=${searched}`)
-              let data: SearchResponse = await cached('search', async () => {
-                return (await fetch(endpoint)).json()
-              })
-              let newSeries: Content[] = []
-              let newMovies: Content[] = []
-              let newEpisodes: Content[] = []
-              data.Search.forEach((result: Content) => {
-                if (result.Type === Format.Series) newSeries.push(result)
-                else if (result.Type === Format.Movie) newMovies.push(result)
-                else newEpisodes.push(result)
-              })
-              setSerieses(newSeries)
-              setMovies(newMovies)
-              setEpisodes(newEpisodes)
-              console.log('data is:', data)
-            } catch (error) {
-              console.error('Error:', error.message)
+    <MemoryRouter>
+      <main className="App">
+        <header className="App-header">
+          <h1>raz</h1>
+        </header>
+        <input
+          type="text"
+          className="App-search"
+          placeholder="Find movies, shows, and people..."
+          onKeyUp={async (event): Promise<void> => {
+            let key: string = event.key
+            let search: string = event.currentTarget.value
+            if (search.length <= 1) {
+              setSerieses([])
+              setMovies([])
+              setEpisodes([])
+              setSearch('')
+            } else if (key === 'Enter') {
+              route('/')
+              try {
+                let { Search, totalResults }: SearchResponse = await cached(search, async () => {
+                  let endpoint = createUrl(`&s=${search}`)
+                  return (await fetch(endpoint)).json()
+                })
+                setSearch(search)
+                setTotal(+totalResults)
+                let newSeries: Content[] = []
+                let newMovies: Content[] = []
+                let newEpisodes: Content[] = []
+                Search.forEach((result: Content) => {
+                  if (result.Type === Format.Series) newSeries.push(result)
+                  else if (result.Type === Format.Movie) newMovies.push(result)
+                  else newEpisodes.push(result)
+                })
+                setSerieses(newSeries)
+                setMovies(newMovies)
+                setEpisodes(newEpisodes)
+              } catch (error) {
+                console.error('Error:', error.message)
+              }
             }
+          }}
+        />
+        {search && (
+          <small style={{ padding: '24px' }}>
+            {total} results for: {search}
+          </small>
+        )}
+        <Route
+          exact
+          path="/"
+          render={(): ReactNode =>
+            display && (
+              <>
+                <ContentContainer contents={movies} format={Format.Movie} />
+                <ContentContainer contents={serieses} format={Format.Series} />
+                <ContentContainer contents={episodes} format={Format.Episode} />
+              </>
+            )
           }
-        }}
-      />
-      {display && (
-        <>
-          <ContentContainer contents={movies} format={Format.Movie} />
-          <ContentContainer contents={serieses} format={Format.Series} />
-          <ContentContainer contents={episodes} format={Format.Episode} />
-        </>
-      )}
-    </main>
+        />
+        <Route exact path="/:type/:imdbID" component={FullContentView} />
+      </main>
+    </MemoryRouter>
   )
 }
